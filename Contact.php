b8866721d16df478506a2b0cb2c1250677fd0000
bristolbagpiper <85064083+bristolbagpiper@users.noreply.github.com>
2021-05-30 15:15:23 +0100

    website

---
+++ Contact.php
@@ -0,0 +1,391 @@
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "https://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+<html xmlns="https://www.w3.org/1999/xhtml">
+<head>
+<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+<title>Hire Bristol Bagpiper Wedding- Contact us</title>
+<script   src="https://code.jquery.com/jquery-3.1.1.min.js"   integrity="sha256-hVVnYaiADRTO2PzUGmuLJr8BLUSjGIZsDYGmIJLv2b8="   crossorigin="anonymous"></script>
+<link rel="stylesheet" href="https://ajax.googleapis.com/ajax/libs/jqueryui/1.12.1/themes/smoothness/jquery-ui.css">
+<script src="https://ajax.googleapis.com/ajax/libs/jqueryui/1.12.1/jquery-ui.min.js"></script>
+<link href="https://fonts.googleapis.com/css?family=Adamina|Droid+Serif|Gentium+Basic|Nobile|Proza+Libre|Slabo+27px" rel="stylesheet">
+<link href='https://fonts.googleapis.com/css?family=Oswald:300' rel='stylesheet' type='text/css'>
+<meta name="description" content="Bristol Bagpiper for hire - Contact">
+<link href="https://fonts.googleapis.com/css?family=Lora|Merriweather" rel="stylesheet">
+<meta name="viewport" content="user-scalable = yes">
+
+
+
+
+
+
+<style type="text/css">
+.Heading {
+	font-family: Verdana, Geneva, sans-serif;
+	font-size: 50px;
+	text-align: center;
+}
+
+
+
+.fade-in{
+  animation: animationFrames ease 1s;
+  animation-iteration-count: 1;
+  transform-origin: 50% 50%;
+  animation-fill-mode:forwards; /*when the spec is finished*/
+  -webkit-animation: animationFrames ease 1s;
+  -webkit-animation-iteration-count: 1;
+  -webkit-transform-origin: 50% 50%;
+  -webkit-animation-fill-mode:forwards; /*Chrome 16+, Safari 4+*/ 
+  -moz-animation: animationFrames ease 1s;
+  -moz-animation-iteration-count: 1;
+  -moz-transform-origin: 50% 50%;
+  -moz-animation-fill-mode:forwards; /*FF 5+*/
+  -o-animation: animationFrames ease 1s;
+  -o-animation-iteration-count: 1;
+  -o-transform-origin: 50% 50%;
+  -o-animation-fill-mode:forwards; /*Not implemented yet*/
+  -ms-animation: animationFrames ease 1s;
+  -ms-animation-iteration-count: 1;
+  -ms-transform-origin: 50% 50%;
+  -ms-animation-fill-mode:forwards; /*IE 10+*/
+}
+
+@keyframes animationFrames{
+  0% {
+    opacity:0;
+    transform:  translate(0px,-25px)  ;
+  }
+  100% {
+    opacity:1;
+    transform:  translate(0px,0px)  ;
+  }
+}
+
+@-moz-keyframes animationFrames{
+  0% {
+    opacity:0;
+    -moz-transform:  translate(0px,-25px)  ;
+  }
+  100% {
+    opacity:1;
+    -moz-transform:  translate(0px,0px)  ;
+  }
+}
+
+@-webkit-keyframes animationFrames {
+  0% {
+    opacity:0;
+    -webkit-transform:  translate(0px,-25px)  ;
+  }
+  100% {
+    opacity:1;
+    -webkit-transform:  translate(0px,0px)  ;
+  }
+}
+
+@-o-keyframes animationFrames {
+  0% {
+    opacity:0;
+    -o-transform:  translate(0px,-25px)  ;
+  }
+  100% {
+    opacity:1;
+    -o-transform:  translate(0px,0px)  ;
+  }
+}
+
+@-ms-keyframes animationFrames {
+  0% {
+    opacity:0;
+    -ms-transform:  translate(0px,-25px)  ;
+  }
+  100% {
+    opacity:1;
+    -ms-transform:  translate(0px,0px)  ;
+  }
+}
+
+
+body {
+	background-image:url(untitled.png);
+	background-repeat:no-repeat;
+	background-size:cover;
+	background-attachment:fixed;
+	height:100%;
+	
+}
+
+
+
+#wrap {
+	min-height:1000px;	
+	
+}
+
+
+#text1 {
+	font-size: 20px;
+	font-family: Georgia, "Times New Roman", Times, serif;
+	color: #FFFFFF;
+	display:inline-block;
+	padding:20px;
+	margin-top:5%;
+
+
+}
+
+
+
+.updated {
+	font-size:10px;
+	color:#ffffff;
+	width:15%;
+	height:1%;
+	margin:auto;
+	bottom:0;
+	padding-top:1%;
+	padding-bottom:1%;
+	border-radius:5%;
+	margin-top:5%;
+}
+div.topBotomBordersOut a:before, div.topBotomBordersOut a:after
+{
+    position: absolute;
+    left: 0px;
+    width: 100%;
+    height: 2px;
+    background: #FFF;
+    content: "";
+    opacity: 0;
+    transition: all 0.3s;
+}
+
+div.topBotomBordersOut a:before
+{
+    top: 0px;
+    transform: translateY(10px);
+}
+
+div.topBotomBordersOut a:after
+{
+    bottom: 0px;
+    transform: translateY(-10px);
+}
+
+div.topBotomBordersOut a:hover:before, div.topBotomBordersOut a:hover:after
+{
+    opacity: 1;
+    transform: translateY(0px);
+}
+
+div.container
+{
+    font-family:Raleway;
+    margin: auto;
+	padding-top:3%;
+	text-align: center;
+	display:inline-block;
+	margin-bottom:3%
+
+}
+
+div.container a
+{
+    color: #FFF;
+    text-decoration: none;
+    font:  30px 'Lora', serif;
+    margin: 0px 10px;
+    padding: 10px 10px;
+    position: relative;
+    z-index: 0;
+    cursor: pointer;
+}
+}
+.form-style-1 {
+    margin:10px auto;
+    max-width: 400px;
+    color: #FFFFFF;
+    padding: 20px 12px 10px 20px;
+    font: 13px "Lucida Sans Unicode", "Lucida Grande", sans-serif;
+}
+.form-style-1 li {
+    padding: 0;
+    display: block;
+    list-style: none;
+    margin: 10px 0 0 0;
+}
+.form-style-1 label{
+    margin:0 0 3px 0;
+    padding:0px;
+    display:block;
+    font-weight: bold;
+}
+.form-style-1 input[type=text], 
+.form-style-1 input[type=date],
+.form-style-1 input[type=datetime],
+.form-style-1 input[type=number],
+.form-style-1 input[type=search],
+.form-style-1 input[type=time],
+.form-style-1 input[type=url],
+.form-style-1 input[type=email],
+textarea, 
+select{
+    box-sizing: border-box;
+    -webkit-box-sizing: border-box;
+    -moz-box-sizing: border-box;
+    border:1px solid #BEBEBE;
+    padding: 7px;
+    margin:0px;
+    -webkit-transition: all 0.30s ease-in-out;
+    -moz-transition: all 0.30s ease-in-out;
+    -ms-transition: all 0.30s ease-in-out;
+    -o-transition: all 0.30s ease-in-out;
+    outline: none;  
+}
+.form-style-1 input[type=text]:focus, 
+.form-style-1 input[type=date]:focus,
+.form-style-1 input[type=datetime]:focus,
+.form-style-1 input[type=number]:focus,
+.form-style-1 input[type=search]:focus,
+.form-style-1 input[type=time]:focus,
+.form-style-1 input[type=url]:focus,
+.form-style-1 input[type=email]:focus,
+.form-style-1 textarea:focus, 
+.form-style-1 select:focus{
+    -moz-box-shadow: 0 0 8px #88D5E9;
+    -webkit-box-shadow: 0 0 8px #88D5E9;
+    box-shadow: 0 0 8px #88D5E9;
+    border: 1px solid #88D5E9;
+}
+.form-style-1 .field-divided{
+    width: 49%;
+}
+
+.form-style-1 .field-long{
+    width: 100%;
+}
+.form-style-1 .field-select{
+    width: 100%;
+}
+.form-style-1 .field-textarea{
+    height: 100px;
+}
+.form-style-1 input[type=submit], .form-style-1 input[type=button]{
+    background: #4B99AD;
+    padding: 8px 15px 8px 15px;
+    border: none;
+    color: #fff;
+}
+.form-style-1 input[type=submit]:hover, .form-style-1 input[type=button]:hover{
+    background: #4691A4;
+    box-shadow:none;
+    -moz-box-shadow:none;
+    -webkit-box-shadow:none;
+}
+.form-style-1 .required{
+    color:red;
+}
+form1 {
+	font-size: medium;
+	font-family: Georgia, "Times New Roman", Times, serif;
+	color: #FFFFFF;
+	text-align:left;
+	margin-left:25%;
+	margin-top:5%;
+
+
+}
+
+</style>
+</head>
+<div class="wrapper fade-in">
+
+<body class="Heading">
+<div class="container red topBotomBordersOut">
+  <a href="index.html">HOME</a>
+  <a href="About.html">ABOUT</a>
+  <a href="Gallery.html">GALLERY</a>
+  <a href="Audio.html">AUDIO</a>
+  <a href="Contact.php">CONTACT</a>
+</div>
+
+
+
+<div id="wrap">
+
+
+
+<div id="text1">
+To book Julian for your event, please use the contact form below,
+<br/>
+ by filling out your email and message.
+
+<?php
+
+$action=$_POST['Message'];
+if ($action=="")    /* display the contact form */
+    {
+?>
+    
+<form method="post" action="Contact.php">
+<ul class="form-style-1">
+    <li><label>Full Name <span class="required">*</span></label><input type="text" name="field1" class="field-divided" placeholder="First" />&nbsp;<input type="text" name="field2" class="field-divided" placeholder="Last" /></li>
+    <li>
+        <label>Email<span class="required">*</span></label>
+        <input type="email" name="Email" class="field-long" />
+    </li>
+    <li>
+        <label>Message <span class="required">*</span></label>
+        <textarea name="Message" id="field5" class="field-long field-textarea"></textarea>
+    </li>
+    <li>
+        <input type="submit" value="Submit" />
+    </li>
+</ul>
+</form>
+
+<?php
+    } 
+else                /* send the submitted data */
+    {
+
+    $email=$_POST['Email'];
+    $message=$_POST['Message'];
+    $field1=$_POST['field1'];
+    $field2=$_POST['field2'];
+    if (($email=="")||($message==""))
+        {
+		
+		
+		?>
+		<p style="color:#FFFFFF; font-family:Georgia, "Times New Roman", Times, serif; size:15px;">All fields are required, please fill <a style="text-decoration:none; color:#FFFFFF;" href=\"\">the form</a> again.</p>
+		
+		
+		<?php
+	    
+	    
+	    
+	    
+	    }
+    else{		
+	    $from="From:<$email>\r\nReturn-path: $email";
+        $subject="$field1, $field2";
+		mail("contact@bristolbagpiper.co.uk", $subject, $message, $from);
+		
+		?>
+		<p style="color:#FFFFFF; font-family:Georgia, "Times New Roman", Times, serif; size:15px;">Email sent!</p>
+		
+		
+		<?php
+	    }
+    }  
+?>
+</div>
+
+
+
+</div>
+
+<p class="updated">Website updated 27th July 2019 - 27/07/19</p>
+</div>
+</body>
+</html>
